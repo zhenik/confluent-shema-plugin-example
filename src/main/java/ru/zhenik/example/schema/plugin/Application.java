6226641d596f53ac32f5c9f88636ab35fdfa16ca
NikitaZhevnitskiy <nikita.zhevnitskiy@gmail.com>
2019-02-14 10:30:00 +0100

    init

---
+++ src/main/java/ru/zhenik/example/schema/plugin/Application.java
@@ -0,0 +1,12 @@
+package ru.zhenik.example.schema.plugin;
+
+import java.util.Properties;
+
+public class Application {
+  public static void main(String[] args) {
+    final Properties properties = new Properties();
+
+
+
+  }
+}
